2c90ee515a38d9c98d18baa8e154827f6f521732
John Davies-Colley <johndaviesco@gmail.com>
2016-03-31 00:58:06 +1300

    Added browsing functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
+var uploading = multer();
+router.use(uploading.single('file'));
+
+var cheerio = require('cheerio');
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+
+client.execute("OPEN Colenso");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,8 +20,46 @@ router.get('/search', function (req, res) {
 	res.render('search');
 });
 
-router.get('/browse', function (req, res) {
-	res.render('browse');
+router.get("/browse",function(req,res) {
+
+    var queries = req.query;
+
+    var path = "";
+
+    var depth = 0;
+
+    if (queries.path != undefined) {
+        path = queries.path;
+        depth = path.split('/').length;
+        path = path + '/';
+    }
+
+    client.execute("XQUERY for $p in collection('Colenso/" + path + "') return db:path($p)",
+        function (error, result) {
+            if (error) {
+                console.error(error);
+            } 
+            else {
+                var results = result.result.split('\n');
+                var folders = [];
+                var files = [];
+                for (var i = 0; i < results.length; i += 1) {
+                    if (results[i].split('/')[depth].indexOf('.xml') < 0) {
+                        folders.push(path + results[i].split('/')[depth]);
+                    } 
+                    else {
+                        files.push(results[i].split('/')[depth]);
+                    }
+                }
+                var unique_folders = [];
+                for (var i = 0; i < folders.length; i += 1) {
+                    if (unique_folders.indexOf(folders[i]) < 0) {
+                        unique_folders.push(folders[i]);
+                    }
+                }
+                res.render('browse', {title: 'Some Letters?', path: path, folders: unique_folders, files: files});
+            }
+    })
 });
 
 router.get('/submit', function (req, res) {
