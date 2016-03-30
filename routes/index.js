f47909e06bb568a49cd33a55ded84cff0a4d4895
John Davies-Colley <johndaviesco@gmail.com>
2016-03-30 18:56:56 +1300

    Created corrct page structure

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/search', function (req, res) {
+	res.render('search');
+});
+
+router.get('/browse', function (req, res) {
+	res.render('browse');
+});
+
+router.get('/submit', function (req, res) {
+	res.render('submit');
+});
+
 module.exports = router;
